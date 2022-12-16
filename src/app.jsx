ebb1baf16ffa4628ad79123dcc4e8068c29c4f86
Zuev Vladimir <zuev93@mail.ru>
2022-12-16 14:44:57 +0100

    Adjust app with the Mirotone requirements: Use icons instead of text links. Use radiobutton style for radiobuttons. Explained why other mods are disabled.

--- src/app.jsx
+++ src/app.jsx
@@ -5,18 +5,26 @@ import Calculator from "./calculator";
 ReactDOM.render(
     <StrictMode>
         <div>
-            <input type="radio" name="mode" value="basic" defaultChecked="defaultChecked"/>
-            <label htmlFor="basic">Basic</label>
-            <input type="radio" name="mode" value="scientific" disabled/>
-            <label htmlFor="scientific" title="Coming soon">Scientific</label>
-            <input type="radio" name="mode" value="programmer" disabled/>
-            <label htmlFor="programmer" title="Coming soon">Programmer</label>
+            <label className="radiobutton">
+                <input type="radio" name="mode" defaultChecked="defaultChecked"/>
+                <span>Basic</span>
+            </label>
+            <label className="radiobutton"  title="Coming soon">
+                <input type="radio" name="mode" disabled/>
+                <span>Scientific (Coming soon)</span>
+            </label>
+            <label className="radiobutton"  title="Coming soon">
+                <input type="radio" name="mode" disabled/>
+                <span>Programmer (Coming soon)</span>
+            </label>
         </div>
         <Calculator />
         <div className="footer">
+            <span className="links">
+                <a className="icon icon-help-question" href="https://github.com/zuev93/calculator_miro" target="_blank"></a>
+                <a className="icon icon-comment-feedback" href="https://github.com/zuev93/calculator_miro/issues" target="_blank"></a>
+            </span>
             <span>v0.1</span>
-            <a href="https://github.com/zuev93/calculator_miro" target="_blank">About</a>
-            <a href="https://github.com/zuev93/calculator_miro/issues" target="_blank">Give feedback</a>
         </div>
     </StrictMode>,
     document.getElementById("root")
