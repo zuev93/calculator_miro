bedd33cd7a198ce02af2968e15e96feb9d911b47
Vladimir Zuev <vladimir.z@miro.com>
2023-01-17 14:12:55 +0100

    Shrink modes buttons as well in case of high zoom level.

--- src/app.jsx
+++ src/app.jsx
@@ -1,30 +1,34 @@
-import React, { StrictMode } from "react";
+import React, {StrictMode} from "react";
 import ReactDOM from "react-dom";
 import Calculator from "./calculator";
 
 ReactDOM.render(
     <StrictMode>
         <div className="modes">
-            <label className="radiobutton">
-                <input type="radio" name="mode" defaultChecked="defaultChecked"/>
-                <span>Basic</span>
-            </label>
-            <label className="radiobutton"  title="Coming soon">
-                <input type="radio" name="mode" disabled/>
-                <span>Scientific (Coming soon)</span>
-            </label>
-            <label className="radiobutton"  title="Coming soon">
-                <input type="radio" name="mode" disabled/>
-                <span>Programmer (Coming soon)</span>
-            </label>
+            <div className="modesButtons">
+                <label className="radiobutton">
+                    <input type="radio" name="mode" defaultChecked="defaultChecked"/>
+                    <span>Basic</span>
+                </label>
+                <label className="radiobutton" title="Coming soon">
+                    <input type="radio" name="mode" disabled/>
+                    <span>Scientific (Coming soon)</span>
+                </label>
+                <label className="radiobutton" title="Coming soon">
+                    <input type="radio" name="mode" disabled/>
+                    <span>Programmer (Coming soon)</span>
+                </label>
+            </div>
         </div>
         <div className="calculator">
-            <Calculator />
+            <Calculator/>
         </div>
         <div className="footer">
             <span className="links">
-                <a className="icon icon-help-question" href="https://github.com/zuev93/calculator_miro" target="_blank"></a>
-                <a className="icon icon-comment-feedback" href="https://github.com/zuev93/calculator_miro/issues" target="_blank"></a>
+                <a className="icon icon-help-question" href="https://github.com/zuev93/calculator_miro"
+                   target="_blank"></a>
+                <a className="icon icon-comment-feedback" href="https://github.com/zuev93/calculator_miro/issues"
+                   target="_blank"></a>
             </span>
             <span>v0.2</span>
         </div>
